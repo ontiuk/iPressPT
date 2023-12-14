2984139fac82a0a62e8dc4eae5b51761c2b95144
Stephen Betley <ontiuk@hotmail.com>
2023-12-14 12:42:34 +0000

    Minor updates
    Minor updates & fixes

--- inc/classes/class-ipr-customizer.php
+++ inc/classes/class-ipr-customizer.php
@@ -40,6 +40,7 @@ if ( ! class_exists( 'IPR_Customizer' ) ) :
 
 			//----------------------------------------------------------------
 			// Customizer Support & Layout
+			// ========================================
 			//
 			// - add_theme_support( 'custom-logo' )
 			// - add_theme_support( 'custom-header' )
